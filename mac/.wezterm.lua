61d7b7ba2d216e2e411e4fe73fd06817be2cde27
Taimour Afzal <taimour.rkt@gmail.com>
2025-01-28 23:07:14 +0500

    Create .wezterm.lua

---
+++ mac/.wezterm.lua
@@ -0,0 +1,13 @@
+local wezterm = require 'wezterm'
+
+local config = {}
+
+config.keys = {
+  {
+    key = 'k',
+    mods = 'CMD',
+    action = wezterm.action.ClearScrollback 'ScrollbackAndViewport',
+  },
+}
+
+return config
