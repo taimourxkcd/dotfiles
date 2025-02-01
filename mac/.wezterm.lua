cc22bf6b1adfd6c5b830aec98d9707c431847d6f
taimourxkcd <taimour.rkt@gmail.com>
2025-02-01 14:19:14 +0500

    new files

--- mac/.wezterm.lua
+++ mac/.wezterm.lua
@@ -1,13 +1,36 @@
 local wezterm = require 'wezterm'
 
-local config = {}
+
+local config = {
+};
 
 config.keys = {
-  {
-    key = 'k',
-    mods = 'CMD',
-    action = wezterm.action.ClearScrollback 'ScrollbackAndViewport',
-  },
+
+	{
+		key = '=',
+		mods = 'CTRL',
+		action = wezterm.action("IncreaseFontSize"),
+	},
+	{
+		key = '-',
+		mods = 'CTRL',
+		action = wezterm.action("DecreaseFontSize"),
+	},
+	{
+		key = 'v',
+		mods = 'CTRL',
+		action = wezterm.action({ PasteFrom = "Clipboard" }),
+	},
+	{
+		key = 'c',
+		mods = 'CTRL',
+		action = wezterm.action({ CopyTo = "Clipboard" }),
+	},
+	{
+		key = 'k',
+		mods = 'CMD',
+		action = wezterm.action.ClearScrollback 'ScrollbackAndViewport',
+	},
 }
 
 return config
